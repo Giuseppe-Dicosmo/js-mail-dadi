004891b1c3a999b6fadd4c1f01b0de9552d0c8d2
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-09 17:26:29 +0100

    email

--- js/myscript.js
+++ js/myscript.js
@@ -1,21 +1,35 @@
+// L'utente inserisce la sua email
+let email = prompt('Inserisci la tua email');
+console.log(email);
+
+
 // Lista di email
 const iscritti = [
   'giuseppedicosmo530@gmail.com',
-  'idaricco12@gmail.com',
+  'mariaricco12@gmail.com',
   'francescolupo@libero.it',
   'tizio20@Outlook.com'
 ]
 console.log(iscritti);
 
-// let eMail2 = 'giuseppedicosmo530@gmail.com'
-// console.log(eMail2)
+// Cerca le email.
+let cercaEmail = false
 
-// L'utente inserisce la sua email
-let eMail = prompt('Esci la tua email');
-console.log(eMail);
+// Separa le email.
+for (let i = 0; i < iscritti.length; i++){
+  console.log(i);
 
-if (eMail == iscritti.length) {
-  alert('ok')
+  let iscrizioni = iscritti[i];
+
+  if (email == iscrizioni) {
+    cercaEmail = true
+  }
+
+}
+
+// Risposta del software.
+if (cercaEmail) {
+  alert('Bentornato')
 } else {
-  alert('Sbagliato')
+  alert('Iscriviti')
 }
